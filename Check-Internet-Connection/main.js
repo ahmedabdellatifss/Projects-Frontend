5f0379371d491567119516f7968766980f0836f6
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2022-07-07 14:03:16 +0200

    Check the internet connection by javascript

---
+++ Check-Internet-Connection/main.js
@@ -0,0 +1,44 @@
+
+let title = document.querySelector(".title");
+let ul = document.querySelector("ul");
+let reload = document.querySelector(".reload");
+
+
+window.onload = function(){
+    if(window.navigator.onLine){
+        onLine()
+    }else{
+        offLine()
+    }
+}
+
+
+window.addEventListener("online" , function(){
+    onLine();
+});
+
+window.addEventListener("offline" , function(){
+    offLine();
+})
+
+
+reload.onclick = function (){
+    window.location.reload
+}
+
+
+function onLine(){
+    title.innerHTML = "OnLine Now";
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+
+}
+
+
+function offLine(){
+    title.innerHTML = "OffLine Now";
+    title.style.color = '#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+}
